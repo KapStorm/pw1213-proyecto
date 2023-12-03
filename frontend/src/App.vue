df641dada9b615373f7bdff430b42aa266353eac
Sebastian <seb02tian20@gmail.com>
2023-12-02 17:47:30 -0700

    [Frontend] init

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <router-link to="/">Home</router-link>
+      </nav>
+    </header>
+
+    <router-view/>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+    }
+  },
+}
+</script>
+
+<template>
+  <main>
+    <button @click="increment">Increment</button>
+    <h1>{{count}}</h1>
+  </main>
+</template>
