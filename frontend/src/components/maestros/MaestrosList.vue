123284cd6e8288ea11316da62db93ff04b841454
Sebastian <seb02tian20@gmail.com>
2023-12-09 17:32:02 -0700

    Maestros data-table

--- frontend/src/components/maestros/MaestrosList.vue
+++ frontend/src/components/maestros/MaestrosList.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 defineProps({
   maestros: {
     type: Array,
@@ -9,10 +10,40 @@ defineProps({
     required: true
   }
 })
+
+const headers = [
+  {
+    title: 'Clave Maestro',
+    value: 'clavemaestro'
+  },
+  {
+    title: 'Nombre',
+    value: 'nombre'
+  },
+  {
+    title: 'Departamento',
+    value: 'departamento'
+  },
+  {
+    title: 'Estatus',
+    value: 'estatus'
+  },
+  {
+    title: 'Acciones',
+    value: 'action'
+  }
+]
 </script>
 
 <template>
-  {{ maestros }}
+  <template>
+    <v-data-table :headers='headers' :items='maestros'>
+      <template v-slot:[`item.action`]>
+        <v-icon icon='mdi-pencil' />
+        <v-icon icon='mdi-delete' />
+      </template>
+    </v-data-table>
+  </template>
 </template>
 
 <style scoped>
