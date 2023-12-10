f7a29124f66d9adcf743581fce9ce4c442ec0bc0
Sebastian <seb02tian20@gmail.com>
2023-12-10 12:39:50 -0700

    [Frontend] Grupos list

---
+++ frontend/src/components/grupos/GruposList.vue
@@ -0,0 +1,73 @@
+<script setup>
+
+defineProps({
+  grupos: {
+    type: Array,
+    required: true
+  },
+  getGrupos: {
+    type: Function,
+    required: true
+  }
+})
+
+const headers = [
+  {
+    title: 'Clave Grupo',
+    value: 'clavegrupo'
+  },
+  {
+    title: 'Clave Materia',
+    value: 'clavemateria'
+  },
+  {
+    title: 'Clave Maestro',
+    value: 'clavemaestro'
+  },
+  {
+    title: 'Limite Alumnos',
+    value: 'limitealumnos'
+  },
+  {
+    title: 'Inscritos',
+    value: 'inscritos'
+  },
+  {
+    title: 'Horario Lunes',
+    value: 'horariolunes'
+  },
+  {
+    title: 'Horario Martes',
+    value: 'horariomartes'
+  },
+  {
+    title: 'Horario Miercoles',
+    value: 'horariomiercoles'
+  },
+  {
+    title: 'Horario Jueves',
+    value: 'horariojueves'
+  },
+  {
+    title: 'Horario Viernes',
+    value: 'horarioviernes'
+  },
+  {
+    title: 'Acciones',
+    value: 'action'
+  }
+]
+</script>
+
+<template>
+  <v-data-table :headers='headers' :items='grupos'>
+    <template #[`item.action`]>
+      <v-icon icon='mdi-pencil' />
+      <v-icon icon='mdi-delete' />
+    </template>
+  </v-data-table>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/views/GruposView.vue
+++ frontend/src/views/GruposView.vue
@@ -1,7 +1,16 @@
 <script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+import GruposList from '@/components/grupos/GruposList.vue'
 
+const grupos = ref([])
+
+async function getGrupos() {
+  const response = await axios.get('http://localhost:3000/grupos')
+  grupos.value = response.data
+}
 </script>
 
 <template>
-  <h1>Grupos view</h1>
+  <GruposList :get-grupos='getGrupos' :grupos='grupos' />
 </template>
