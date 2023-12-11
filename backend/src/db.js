6dc46f3475ac54728ecd780e8031550465c15c98
Sebastian <seb02tian20@gmail.com>
2023-12-10 21:12:09 -0700

    [Backend] db para xampp

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,19 +1,19 @@
 import mysql from 'mysql2'
 
 // ESTA ES LA CONFIG DE LA BD USANDO DOCKER
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'admin',
-    password: 'admin',
-    database: 'pw',
-    port: 3306
-})
-
 // const db = mysql.createPool({
 //     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'pw1213',
+//     user: 'admin',
+//     password: 'admin',
+//     database: 'pw',
+//     port: 3306
 // })
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pw1213',
+})
 
 export default db
